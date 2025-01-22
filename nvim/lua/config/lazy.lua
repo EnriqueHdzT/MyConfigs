47007cded407c62ceb760218b2eb8f5fea6ea716
Enrique Hernández <ehernandez@iceiot.com.mx>
2025-01-22 17:35:50 -0600

    Adds new features to nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -15,6 +15,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 
 -- Vim Config
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 vim.opt.rtp:prepend(lazypath)
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -23,10 +26,22 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>', { desc = 'Reveal filesystem in Neotree' })
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", { desc = "Reveal filesystem in Neotree" })
+vim.keymap.set("n", "<C-h>", "<C-w>h<CR>", { desc = "Move to window on the left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j<CR>", { desc = "Move to window below" })
+vim.keymap.set("n", "<C-k>", "<C-w>k<CR>", { desc = "Move to window above" })
+vim.keymap.set("n", "<C-l>", "<C-w>l<CR>", { desc = "Move to window on the right" })
+vim.keymap.set("n", "<C-t>", ":tab terminal<CR>", { desc = "Open new terminal" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
+vim.keymap.set("n", "<leader>fmt", vim.lsp.buf.format, { desc = "Format current file" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show function information" })
+vim.keymap.set("n", "gd", function()
+  vim.cmd("set splitright")
+  vim.cmd("vsplit")
+  vim.lsp.buf.definition()
+end, { desc = "Open function definition in a split window" })
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.keymap.set("t", "<C-space>", "<C-\\><C-n>", { desc = "Escape terminal mode" })
 
 -- Import plugins
 require("lazy").setup({
---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "bashls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.bashls.setup({})
+    end,
+  },
+}
